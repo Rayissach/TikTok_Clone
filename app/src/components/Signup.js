8aaebf104f54cd7d76cf0ed920fb2c3564d59abf
Raymond Robinson <rayissach@gmail.com>
2024-08-26 22:15:56 -0400

    Creating form elements for Signup component

---
+++ app/src/components/Signup.js
@@ -0,0 +1,26 @@
+import React from 'react'
+
+export const Signup = () => {
+    return (
+        <div>
+            <form >
+                <label for='firstName'>
+                    First Name:
+                </label>
+                <label for='lastName'>
+                    Last Name:
+                </label>
+                <label for='email'>
+                    Email:
+
+                </label>
+                <label for='username'>
+                    Username:
+                </label>
+                <label for='password'>
+                    Password:
+                </label>
+            </form>
+        </div>
+    )
+}
