b9bd1b234f258eb7fc0dbbeeb8b229023a442d09
Raymond Robinson <rayissach@gmail.com>
2024-08-26 22:10:32 -0400

    Created jsx expression to export Signup and Login components

--- app/src/components/Profile.js
+++ app/src/components/Profile.js
@@ -1,9 +1,14 @@
 import React from 'react'
+import { Nav } from './Nav'
 
 export const Profile = () => {
     return (
         <div>
             <Nav />
+            <img />
+            <p>Username</p>
+            
+
         </div>
     )
 }
