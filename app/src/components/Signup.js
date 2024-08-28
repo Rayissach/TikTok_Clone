968584769e08536d76a12220f20532be76494031
Raymond Robinson <rayissach@gmail.com>
2024-08-27 21:14:47 -0400

    Adding name attribute and input to form data

--- app/src/components/Signup.js
+++ app/src/components/Signup.js
@@ -1,29 +1,61 @@
 import React, {useState} from 'react'
 
 export const Signup = () => {
+    const [data, setData] = {
+        'email': '',
+        'firstName' : '',
+        'lastName' : '',
+        'username' : '',
+        'password' : ''
+    }
+    
     return (
         <div>
             <form >
                 <label for='firstName'>
                     First Name:
                     <input
-                        value={}
+                        name='firstName'
+                        value={data.firstName}
                         onChange={}
                         onSubmit={}
                     />
                 </label>
                 <label for='lastName'>
                     Last Name:
+                    <input 
+                        name='lastName'
+                        value={data.lastName}
+                        onChange={}
+                        onSubmit={}
+                    />
                 </label>
                 <label for='email'>
                     Email:
-
+                    <input 
+                        name='email'
+                        value={data.email}
+                        onChange={}
+                        onSubmit={}
+                    />
                 </label>
                 <label for='username'>
                     Username:
+                    <input 
+                        name='username'
+                        value={data.username}
+                        onChange={}
+                        onSubmit={}
+                    />
                 </label>
                 <label for='password'>
                     Password:
+                    <input 
+                        name='password'
+                        value={data.password}
+                        onChange={}
+                        onSubmit={}
+                    />
                 </label>
             </form>
         </div>
